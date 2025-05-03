b9067c6c39ccf229a56f545da9e052b0d87a4be1
Bennie09 <benjaminoguntade09@gmail.com>
2025-05-03 11:18:10 +0100

    change

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
-import { FiInfinity } from 'react-icons/fi';
+import { LiaInfinitySolid } from "react-icons/lia";
 
 export default function Logo() {
   return (
     <div className="flex items-center space-x-2">
-      <FiInfinity className="h-8 w-8 text-primary-600 dark:text-primary-400" />
+      <LiaInfinitySolid className="h-8 w-8 text-primary-600 dark:text-primary-400" />
       <span className="text-2xl font-bold text-primary-600 dark:text-primary-400">Infinite</span>
     </div>
   );
